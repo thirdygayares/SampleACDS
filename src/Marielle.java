736d5e372a384f23021b70889f1d9db4c80a7cf1
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-14 20:03:21 +0800

    update marielle.class

--- src/Marielle.java
+++ src/Marielle.java
@@ -2,6 +2,6 @@
 public class Marielle {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Thirdyy.");
+        System.out.println("I love you, Thirdyy.");
     }
 }
