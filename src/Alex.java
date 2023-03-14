9bc0332d136a8e894bf37af66cdbb5c4275789e9
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-14 20:00:03 +0800

    update task

--- src/Alex.java
+++ src/Alex.java
@@ -8,7 +8,6 @@ public class Alex {
 		System.out.println("Hello Alex!");
 		
 		
-
 	}
 	
 	
--- src/Thirdy.java
+++ src/Thirdy.java
@@ -3,7 +3,9 @@ public class Thirdy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+			
+		//Print
+		System.out.print("Hello Thirdy");
 	}
 
 }
