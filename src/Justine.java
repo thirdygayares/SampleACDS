672517f97ba0d91d20424659b90731bfaded20ed
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-03-14 16:09:30 +0800

    add classmates
    - elaborate

---
+++ src/Justine.java
@@ -0,0 +1,9 @@
+
+public class Justine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Raiden.java
@@ -0,0 +1,9 @@
+
+public class Raiden {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
