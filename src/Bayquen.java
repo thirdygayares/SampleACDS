50695b8f4afea59b797bfd78fa3ba37dd9d3829c
Howard <83273907+Negative-Newspaper@users.noreply.github.com>
2023-03-16 22:03:06 +0800

    Update Bayquen.java

--- src/Bayquen.java
+++ src/Bayquen.java
@@ -4,6 +4,8 @@ public class Bayquen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Howard");
+		
+		System.out.println("eyyy -Howard");
 
 	}
 
