8d8d06d9c94d2cd548405cef279654ec941ebb78
joricel <jroldanbacani@gmail.com>
2023-03-20 21:29:22 +0800

    add commit

--- src/Joricel.java
+++ src/Joricel.java
@@ -4,6 +4,9 @@ public class Joricel {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi Thirdy");
+		
+		//another commit
+		System.out.println("and hello everyone");
 	}
 
 }
