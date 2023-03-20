4c89e74bdbd29e13e7cfe52ec9673798377f5809
JO <83831127+jonny1221@users.noreply.github.com>
2023-03-20 23:47:35 +0800

    Update Jonny Class
    Nag print ulit

--- src/Jonny.java
+++ src/Jonny.java
@@ -4,6 +4,10 @@ public class Jonny {
 	public static void main(String[] args) {
 
 		System.out.println("This is Jonny's class");
+	
+		//second commit for fetching 
+		System.out.println("edi sayo");
+
 	}
 
 }
