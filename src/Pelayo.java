8d25f591e0578124f888203712765bad7d863505
christinezzz29 <83915794+christinezzz29@users.noreply.github.com>
2023-03-22 10:19:04 +0800

    update the code
    print hello for second commit

--- src/Pelayo.java
+++ src/Pelayo.java
@@ -4,6 +4,9 @@ public class Pelayo {
 	public static void main(String[] args) {
 
 		System.out.println("Hello from the other side");
+		
+		//second commit for fetch and pull
+		System.out.println("hello");
 
 }
 	
